81f99bfbbdefd49f9243346ba06481ae835cdd17
Shashwat <shashwat.pandey@leviossa.com>
2015-03-26 14:59:05 +0530

    database name is changed to flikbuk on server

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
-			'database'  => 'blogapp',
+			'database'  => 'flikbuk',
 			'username'  => 'root',
 			'password'  => 'admin123',
 			'charset'   => 'utf8',
