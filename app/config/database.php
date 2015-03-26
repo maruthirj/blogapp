abe1c66a77b7a2941b87c6a0a94f4b5f91dba5c9
Shashwat <shashwat.pandey@leviossa.com>
2015-03-26 14:38:39 +0530

    db password setup for server

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => '127.0.0.1',
 			'database'  => 'blogapp',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'admin123',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
