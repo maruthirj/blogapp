c6d2abe8e10a8f72d2096b43eee2dc511c3fdb62
Shashwat <shashwat.pandey@leviossa.com>
2015-03-28 21:14:34 +0530

    Code commit for Facebook share

--- app/views/welcome.php
+++ app/views/welcome.php
@@ -35,19 +35,35 @@ function searchTag(){
    return false;
 }
 </script>
+<div id="fb-root"></div>
+<script>
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 	<form action="" name="form" id="form">
 		<input type="text" name="search" id="search"/><input type="button" value="Search" onclick="searchTag()"/>
 	</form>
 	<div class="left">
 
 	</div>
+	
 	<div class="imagePanel">
 		<div id="flipbook">
 			<div  style="background: white;">
 			<?php include 'postView.php'?>
 			</div>
 		</div>
+		<div id="fb_share" class="fb-like" data-href="http://www.flikbuk.com/<?php echo $post->post_key?>" data-layout="standard" data-action="like" data-show-faces="true" 
+	       data-share="true"></div>
+		 <br/><br/>
+		 
 		<button id="copy-button">Copy Link</button>
+		<br/>
 	</div>
 	  <div class="tags">
 
