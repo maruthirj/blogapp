aa984bca1399a24dc05376b6e4a209e82e5d03d5
Shashwat <shashwat.pandey@leviossa.com>
2015-04-30 13:17:07 +0530

    customized error page

---
+++ app/views/errors.php
@@ -0,0 +1,47 @@
+<head>
+	<meta charset="UTF-8">
+	<title>Flikbuk</title>
+	<link rel="icon" href="img/favicon.png" type="image/x-icon"/>
+	<link href="/components/jquery-ui/themes/smoothness/jquery-ui.css" type="text/css" rel="stylesheet"/>
+	<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap/bootstrap-theme.min.css">
+	<script src="scripts/js/jquery.min.js"></script>
+	<script src="scripts/js/bootstrap/bootstrap.min.js"></script>
+	<style>
+		@import url(//fonts.googleapis.com/css?family=Lato:700);
+
+		body {
+			margin:0;
+			font-family:'Lato', sans-serif;
+			color: #999;
+		}
+
+		a, a:visited {
+			text-decoration:none;
+		}
+
+		h1 {
+			font-size: 32px;
+			margin: 16px 0 0 0;
+		}
+		.container-full {
+		  margin: 0 auto;
+		  width: 100%;
+		}
+	</style>
+<script type="text/javascript" src="/public/scripts/welcome.js"></script>
+</head>
+<div class="container-full">
+<div class="row">
+	<!-- Logo -->
+	<div class="col-md-3"><img src="img/logo.png" /></div>
+	</div>
+<div class="row">
+<div class="col-md-2"></div>
+<div class="col-md-9">
+<?php
+echo "Whoops, Something went Wrong!";
+?>
+</div>
+</div>
+</div>
