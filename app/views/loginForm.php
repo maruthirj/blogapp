a69036bf89e9ba4f89b26fedc20b4c9e59e6bd86
Shashwat <shashwat.pandey@leviossa.com>
2015-05-12 19:46:37 +0530

    Added Register link on login page

--- app/views/loginForm.php
+++ app/views/loginForm.php
@@ -4,6 +4,7 @@
 <link href="http://www.flikbuk.com/css/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css">
 
 <body>
+
 <div class="top">
 </div>
 <div class="container">
@@ -28,9 +29,9 @@
 	 
 	<div class="logpanel">
     <h1> Login here </h1>
-    <?php echo Form::open(array('url' => '/login', 'class' => 'box login')); ?>
-    <?php
-if(isset($errors)){
+	<?php echo Form::open(array('url' => '/login', 'class' => 'box login')); ?>
+<?php
+	if(isset($errors)){
 	foreach ($errors->all('<p class="wTxt">:message</p>') as $message)
 	{
 	    echo $message;
@@ -41,16 +42,28 @@ if(isset($errors)){
 		<label>User Email : </label> <input class="loginTxtbx" type="text" tabindex="1" name="email" required><br>
 		<label>Password : </label> <input class="loginTxtbx" type="password" name="password" tabindex="2" required>
 	</fieldset>
-	<a href="#" class="rLink" tabindex="5">Forget your password?</a>
+	<a href="#" class="rLink" tabindex="5"  style="padding: 0 0 0 103px;">Forgot password?</a>
+	<a href="/signup" class="rLink" tabindex="5"  style="padding: 0 0 0 6px;"> | Register</a> 
 	<footer>
 		<label><input type="checkbox" name="persist" tabindex="3">Remember me</label>
 		<input type="submit" class="btnLogin" value="Login" tabindex="4" >
 	</footer>
 	</form>
+
+<div id="dialog" style="width:30px;height:60px;display: none;z-index:10000" title="Forgot Password">
+	<table style="width:30%;border: 0px">
+		 <tr>
+		  <td style="font-size:14px;border: 0px;">Enter Email</td>
+		  <td style="border: 0px"><input style="margin-left:30px;" type="text" size="28" id="mailId" name="mailId"> </td>
+		 </tr>
+		 <tr>
+		  <td style="border: 0px"><input style="font-size:18px;margin-top:20px;" type="button" onclick="submit();" value="Submit"> </td>
+		  <td style="font-size:14px;border: 0px;"></td>
+		 </tr>
+	</table>
+</div>
 	</div>
 </div>
-
-
 
 </body>
 
