0cd4cf9bbb6dbb345c76dbd0a7db49636c6cb6de
Maruthi <maruthi@leviossa.com>
2015-07-11 19:54:04 +0530

    FB share correction

--- app/views/postView.php
+++ app/views/postView.php
@@ -12,10 +12,11 @@
 <input type="hidden" id="postId" value="<?php echo $post->id ?>"/>
 <div class="postText">
 <?php echo $post->post_text ?>
-	<div>
-<!-- 			<div class="fb-share-button" data-href="http://www.flikbuk.com/login" data-layout="button"></div> -->
-	        <a class="socialicon1" href="https://www.facebook.com/login.php?next=https%3A%2F%2Fwww.facebook.com%2Fsharer%2Fsharer.php%3Fapp_id%3D309437425817038%26sdk%3Djoey%26u%3Dhttp%253A%252F%252Fwww.flikbuk.com%2Fimg%2Fcontent%2F<?php echo $post->post_key ?>%26display%3Dpopup%26ref%3Dplugin%26src%3Dshare_button%26ret%3Dlogin&display=popup" target="_blank"></a>
-	        <a class="socialicon2" href="https://twitter.com/intent/tweet?original_referer=http%3A%2F%2Fwww.flikbuk.com%2F<?php echo $post->post_key ?>&related=twitterdev&text=Flikbuk&tw_p=tweetbutton&url=http%3A%2F%2Fwww.flikbuk.com%2F" target="_blank"></a>
+</div>
+<div>
+<?php
+                $keyWithoutExtn = preg_replace("#\\..*#","",$post->post_key);
+?>
 			<input type="hidden" id="ratingVal" value="0"></input>
 			
 				<span style="float: left;"><img id="img1" src="img/star_empty.png"
@@ -35,9 +36,10 @@
 				<span style="float: left;"><img id="img5" src="img/star_empty.png"
 					onMouseOver="fillImg(5)" onMouseOut="normalImg(5)"
 					onClick="giveRating(5)" height="42" width="42"></span>
-	</div>	
-	<input type="hidden" value="http://<?php echo $_SERVER['HTTP_HOST']?>/<?php echo $post->post_key?>" name="postLink" id="postLinkInput"/>
-</div>
+				<a class="socialicon1" href="https://www.facebook.com/sharer/sharer.php?sdk=joey&u=http%3A%2F%2Fwww.flikbuk.com%2F<?php echo $keyWithoutExtn ?>&display=popup&ref=plugin&src=share_button" target="_blank"></a>
+	        	<a class="socialicon2" href="https://twitter.com/intent/tweet?original_referer=http%3A%2F%2Fwww.flikbuk.com%2F<?php echo $keyWithoutExtn ?>&related=twitterdev&text=Flikbuk&tw_p=tweetbutton&url=http%3A%2F%2Fwww.flikbuk.com%2F" target="_blank"></a>
+</div>	
+<input type="hidden" value="http://<?php echo $_SERVER['HTTP_HOST']?>/<?php echo $post->post_key?>" name="postLink" id="postLinkInput"/>
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
